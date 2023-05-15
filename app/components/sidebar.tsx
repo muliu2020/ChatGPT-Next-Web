062c1fc720a3435502b87a6a6bbf62f2e43f4583
muliu2020 <125067544+muliu2020@users.noreply.github.com>
2023-05-15 09:43:46 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT 比诗顿</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          仅用于比诗顿内部员工.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
